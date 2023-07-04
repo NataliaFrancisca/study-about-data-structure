1bf2c3d12c5fb9434320fecb115925f4c8374d2c
Natalia Francisca <nathifrancisca@gmail.com>
2023-07-03 21:30:15 -0300

    feat: creating method to remove a node in a linked list

--- double-linked-list.js
+++ double-linked-list.js
@@ -25,6 +25,25 @@ class Playlist{
         }
     }
 
+    // remove the first element at the list
+    deleteHead(){
+        if(!this.head){
+            return null;
+        }else{
+            let removedHead = this.head;
+            // se só existe 1 elemento na minha lista, ele é head/tail => então preciso definir os dois como null
+            if(this.head === this.tail){
+                this.head = this.tail = null;
+            }else{
+                this.head = this.head.next;
+                this.head.prev = null;
+            }
+
+            return removedHead;
+        }
+    }
+
+    // remove the last element at the list
     deleteTail(){
         if(!this.tail){
             return null;
@@ -41,6 +60,42 @@ class Playlist{
 
             return removedTail;
         }
+    }
+
+    deleteNode(value){
+        let currentNode = this.head;
+
+        while(currentNode){
+            if(currentNode.value === value){
+                const prev = currentNode.prev;
+                const next = currentNode.next;
+
+                if(!prev && !next){
+                    this.head = this.tail = null;
+                }
+
+                if(prev && next){
+                    prev.next = next;
+                    next.prev = prev;
+                }
+                
+                if(prev == null){
+                    next.prev = null;
+                    this.head = next;
+                }
+
+                if(next == null){   
+                    prev.next = null
+                    this.tail = prev;
+                }
+
+                return currentNode;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
     }
 
     print(){
@@ -93,7 +148,28 @@ nt_playlist.append('Into In')
 // console.log(nt_playlist.head);
 // console.log(nt_playlist)
 
-
-nt_playlist.deleteTail();
-nt_playlist.print();
+// nt_playlist.deleteTail();
+// nt_playlist.deleteHead();
+// nt_playlist.print();
+// nt_playlist.deleteNode('Youth')
+// nt_playlist.print();
+// nt_playlist.deleteNode('20 ligações');
+// nt_playlist.print();
+// nt_playlist.deleteNode('Into In');
+// nt_playlist.print();
 // nt_playlist.search('Into In');
+
+// delete the second element
+// nt_playlist.deleteNode('Youth');
+
+// delete the third element
+// nt_playlist.deleteNode('20 ligações');
+
+// delete the last element using delete node
+// nt_playlist.deleteNode('Into In');
+
+// delete the first element using delete node
+nt_playlist.deleteNode('Medicine');
+
+// nt_playlist.deleteNode('Medicine');
+nt_playlist.print();
